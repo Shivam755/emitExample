e4ab919c6aeac93489e409e5dbcb0e2d320127b6
Shivam Gupta <66762938+Shivam755@users.noreply.github.com>
2022-05-03 19:53:14 +0530

    first commit

---
+++ components/Form.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="flex flex-col justify-center items-center rounded-lg bg-cyan-300 w-fit">
+        <input class="m-2 pl-3 py-2 outline-0 text-lime-600 text-3xl bg-emerald-200 border-2 border-lime-600 rounded" placeholder="Your name" type="text" name="Name" id="name" v-model="custName" @keyup="updateLabel" />
+        <button class="m-2 rounded outline-0 text-3xl bg-lime-600 text-emerald-200 p-3" @click.prevent="submit">Submit</button>
+    </div>
+</template>
+
+<script lang="ts">
+
+
+export default {
+  name: 'Form',
+  data:()=>{
+      return {
+          custName:''
+      }
+  },
+  methods:{
+      submit(){
+          this.updateLabel()
+          this.custName=''
+      },
+      updateLabel(){
+          this.$emit('name-fetch',this.custName)
+      }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="h-screen flex flex-col justify-center items-center bg-rose-100" >
+        <h1 class="text-5xl text-purple-700 m-4">Hello {{cName}}</h1>
+        <Form @name-fetch='greet' />
+    </div>
+</template>
+
+<script lang="ts">
+import Form from '@/components/Form.vue'
+
+export default {
+  name: 'IndexPage',
+  components:{
+      Form
+  },
+  data(){
+      return{
+          cName:''
+      }
+  },
+  methods:{
+      greet(e){
+          console.log(e)
+          this.cName=e
+      },
+  },
+}
+</script>
